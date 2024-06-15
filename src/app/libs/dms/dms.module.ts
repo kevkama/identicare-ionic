c141c6d5aa8a2c528f76fe18ed4c5ae80f04d719
kevkama <nkamauk7@gmail.com>
2024-06-15 15:11:26 +0300

    msg list design

--- src/app/libs/dms/dms.module.ts
+++ src/app/libs/dms/dms.module.ts
@@ -2,13 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { DmsRoutingModule } from './dms-routing.module';
+import { IonicModule } from '@ionic/angular';
+import { MessageListComponent } from './message-list/message-list.component';
+import { NewMsgComponent } from './new-msg/new-msg.component';
+import { SingleThredComponent } from './single-thred/single-thred.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [MessageListComponent, NewMsgComponent,SingleThredComponent],
   imports: [
     CommonModule,
-    DmsRoutingModule
+    DmsRoutingModule,
+    IonicModule
   ]
 })
 export class DmsModule { }
